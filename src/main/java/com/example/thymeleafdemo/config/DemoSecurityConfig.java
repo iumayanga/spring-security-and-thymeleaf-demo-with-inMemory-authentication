bb502ddd710300e796a278bdfa4e484fd7354d9d
iumayanga <pl.isuru97@gmail.com>
2022-10-04 05:43:54 +0530

    Initial Commit

---
+++ src/main/java/com/example/thymeleafdemo/config/DemoSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.example.thymeleafdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		// add our users for in memory authentication
+		
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+			.withUser(users.username("employee").password("test123").roles("EMPLOYEE"))
+			.withUser(users.username("manager").password("test123").roles("EMPLOYEE", "MANAGER"))
+			.withUser(users.username("admin").password("test123").roles("EMPLOYEE", "ADMIN"));
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+			.antMatchers("/employees/showForm*").hasAnyRole("MANAGER", "ADMIN")
+			.antMatchers("/employees/save*").hasAnyRole("MANAGER", "ADMIN")
+			.antMatchers("/employees/delete").hasRole("ADMIN")
+			.antMatchers("/employees/**").hasRole("EMPLOYEE")
+//			.antMatchers("/resources/**").permitAll()
+			.and()
+			.formLogin()
+				.loginPage("/showMyLoginPage")
+				.loginProcessingUrl("/authenticateTheUser")
+				.permitAll()
+			.and()
+			.logout().permitAll()
+			.and()
+			.exceptionHandling().accessDeniedPage("/access-denied");
+		
+	}
+		
+}
+
+
+
+
+
+
